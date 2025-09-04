ba92f65c3eac7f7c6187a9e507636e8429629919
Jesus Miguel Dulfo <jesusmigueldulfo@gmail.com>
2025-09-04 13:05:08 +0800

    leetcode solutions update 09/04/2025 13:05:08

---
+++ typescript/0001-two-sum.ts
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=array&
+
+function twoSum(nums: number[], target: number): number[] {
+  const indexMap = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    const needed = target - current;
+
+    if (indexMap.has(needed)) {
+      return [indexMap.get(needed)!, i];
+    }
+
+    indexMap.set(current, i);
+  }
+
+  return [];
+}
